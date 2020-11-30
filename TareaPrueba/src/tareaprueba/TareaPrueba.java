bc7771c68e75bf34f98f23c1abcc433d392565fe
Sebastian <sebastian.galdames55@gmail.com>
2020-11-30 15:37:50 -0300

    Inicio Proyecto

--- TareaPrueba/src/tareaprueba/TareaPrueba.java
+++ TareaPrueba/src/tareaprueba/TareaPrueba.java
@@ -5,17 +5,33 @@
  */
 package tareaprueba;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
- * @author nestor
+ * @author sebas
  */
-public class TareaPrueba {
+public class TareaPrueba extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
