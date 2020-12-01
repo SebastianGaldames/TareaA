d1731035eb5f06edacadd99bd157454e58208b37
Sebastian <sebastian.galdames55@gmail.com>
2020-12-01 20:20:03 -0300

    entrega

--- TareaPrueba/src/ControladorInterfaz/FXMLDocumentController.java
+++ TareaPrueba/src/ControladorInterfaz/FXMLDocumentController.java
@@ -17,18 +17,9 @@ import javafx.scene.layout.Pane;
  */
 public class FXMLDocumentController implements Initializable {
     
-    String medico;
-    String dentista;
-    String obstetra;
-    String psicologo;
-    String secretaria;
-    String contraseñaM;
-    String contraseñaD;
-    String contraseñaO;
-    String contraseñaP;
-    String contraseñaS;
-    String aux;
-    String auxPass;
+    
+    String  auxString;
+    String auxPassString;
     int temp = 0;
     
     private Label label;
@@ -48,6 +39,16 @@ public class FXMLDocumentController implements Initializable {
     private Pane paneDerivarPaciente;
     @FXML
     private Pane paneAsignarHora;
+    @FXML
+    private Pane paneEspecialista;
+    @FXML
+    private TextField nombrePaciente;
+    @FXML
+    private TextField infoAdicional;
+    @FXML
+    private TextField rutPaciente;
+    @FXML
+    private Pane InscripcionesP;
         
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -56,47 +57,83 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void iniciarSesion(ActionEvent event) {
+        int medico = 1;
+        int dentista= 2;
+        int obstetra= 3;
+        int psicologo= 4;
+        int secretaria= 5;
+        int contraseñaM= 1;
+        int contraseñaD= 2;
+        int contraseñaO= 3;
+        int contraseñaP= 4;
+        int contraseñaS= 5;
         
-<<<<<<< HEAD
+
+        auxString = nombreUsuario.getText();
+        auxPassString = contrasenia.getText();
         
-=======
-        aux = nombreUsuario.getText();
-        auxPass = contrasenia.getText();
+        int aux = Integer.parseInt(auxString);
+        int auxPass = Integer.parseInt(auxPassString);
         
-        if (medico.equals(aux) && contraseñaM.equals(auxPass)){
+        System.out.println(medico+","+contraseñaM);
+        System.out.println(aux+","+auxPass);
+        if (medico==aux && contraseñaM==auxPass){
             temp = 1;
         }
-        if (dentista.equals(aux) && contraseñaD.equals(auxPass)){
+        else if (dentista==aux && contraseñaD==auxPass){
             temp = 2;
         }
-        if (obstetra.equals(aux) && contraseñaO.equals(auxPass)){
+        else if (obstetra==aux && contraseñaO==auxPass){
             temp = 3;
         }
-        if (psicologo.equals(aux) && contraseñaP.equals(auxPass)){
+        else if (psicologo==aux && contraseñaP==auxPass){
             temp = 4;
         }
-        if (secretaria.equals(aux) && contraseñaS.equals(auxPass)){
+        else if (secretaria==aux && contraseñaS==auxPass){
+            System.out.println("iii");
             temp = 5;
         }
-        else{
-            temp = 6;
-        }
+        
+
         
         switch (temp){
             case 1:
+                paneInicio.setVisible(false);
+                paneMedico.setVisible(true);
                 break;
             case 2:
+                
                 break;
             case 3:
                 break;
             case 4:
                 break;
             case 5:
+                System.out.println("ppp");
+                paneInicio.setVisible(false);
+                paneSecretaria.setVisible(true);
                 break;
             case 6:
                 break;
         }
->>>>>>> Nestor
+    }
+
+    @FXML
+    private void inscribirPaciente(ActionEvent event) {
+        paneInicio.setVisible(false);
+        InscripcionesP.setVisible(true);
+    }
+
+    @FXML
+    private void Inscripcion(ActionEvent event) {
+        
+        String nom =nombrePaciente.getText();
+        String inf = infoAdicional.getText();
+        String rutP = rutPaciente.getText(); 
+        
+        System.out.println("paciente inscrito: "+ nom+" "+inf+" "+rutP);
+        
+        
     }
     
 }
